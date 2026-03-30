920a93ed8d75f3cc7febdb69cd7f1027efcecde1
Eyenoon-Nabin <eyenoon28@gmail.com>
2026-03-31 01:16:20 +0600

    setup interface

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center text-purple-600 mt-10">
+        Welcome to DigiTools!
+      </h1>
+    </div>
+  )
+}
+
+export default App
